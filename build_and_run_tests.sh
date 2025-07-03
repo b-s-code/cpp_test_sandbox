3aa2ff6f22b1caea9e9e7d9a7a8b97c8af82c02d
b-s-code <89502375+b-s-code@users.noreply.github.com>
2025-07-03 15:25:15 +0800

    Comments.

--- build_and_run_tests.sh
+++ build_and_run_tests.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Generates C++ code (specifically, the file runner.cpp) from the headers listed.
+# Note that the resulting cpp file also #includes the headers listed.
 python3 ./cxxtest/bin/cxxtestgen --error-printer -o runner.cpp MyTestSuite1.h
 
-# Generate test runner executable.
+# Generate test runner executable.  This is done by compiling the previously
+# generated cpp file.
 CXXTEST=cxxtest # Used to tell compiled where to find cxxtest's headers.
 g++ -o runner ./runner.cpp -I$CXXTEST
 
